40b702a0c940c6c17820757224205eaa5f10c49f
Anto231996 <tony2396@hotmail.it>
2022-03-02 13:42:37 +0100

    correzione

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,7 +4,14 @@
  */
 
 let userFirstNumber = parseInt(prompt('Scegli un numero'))
+document.getElementById("first-number").innerHTML = userFirstNumber;
 let userSecondNumber = parseInt(prompt('Scegli un secondo numero'))
+document.getElementById("second-number").innerHTML = userSecondNumber;
 
-if (userFirstNumber > userSecondNumber) {console.log(userFirstNumber)} 
-else{console.log(userSecondNumber)}
+
+if (userFirstNumber > userSecondNumber) {console.log(userFirstNumber);
+   document.getElementById("higher-number").innerHTML = userFirstNumber;
+}
+else{console.log(userSecondNumber)
+   document.getElementById("higher-number").innerHTML = userSecondNumber
+}
---
+++ jsnack7/script.js
@@ -0,0 +1,15 @@
+/**
+ * Creiamo un array con dieci numeri randomici
+ * calcoliamo la somma di tutti gli elementi
+ * e calcoliamo anche il valore medio
+ */
+
+const num = [];
+let sum = 0;
+ 
+for (let i = 0 ; i < 10 ; i++){
+    num.push(Math.floor(Math.random() * 100 ) );
+    sum = sum + num;
+}
+ 
+console.log(num);
